658766c4a3ecdc99029473c477186b26e3e292ea
DanielMontaudon <danielmontaudon909@live.com>
2025-06-24 03:14:35 -0500

    Enemy Type Generator
    Added Auras to special enemy types and random generator to spawn as special enemy in certain cases

--- Mid Evil/Assets/Scripts/Enemy_Scripts/EnemyAttributes.cs
+++ Mid Evil/Assets/Scripts/Enemy_Scripts/EnemyAttributes.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.AI;
 
 using System.Collections;
+using System.Collections.Generic;
 using INab.Common;
 
 public class EnemyAttributes : MonoBehaviour
@@ -12,6 +13,29 @@ public class EnemyAttributes : MonoBehaviour
     public float enemyDamage = 2f;
     public float enemyAttackSpeed = 1f;
 
+    [SerializeField] private EnemyType enemyType = EnemyType.regular;
+    [SerializeField] private GameObject enemyTypeVFX;
+    /*
+     * Fire - 0
+     * Earth - 1
+     * Wind - 2
+     * Water - 3
+     * Lightning - 4
+     * Arcana - 5
+     */
+    public List<GameObject> enemyAuras = new List<GameObject>();
+    //Different types resemble special infected
+    public enum EnemyType
+    {
+        regular,
+        fire,
+        earth,
+        wind,
+        water,
+        lightning,
+        arcane
+    }
+
     [SerializeField] private Transform maskDissolve;
 
     private bool isDead = false;
@@ -19,12 +43,59 @@ public class EnemyAttributes : MonoBehaviour
 
     private void Start()
     {
+        float enemyTypeGenerator = Random.Range(1, 100);
+        if (enemyTypeGenerator <= 85)
+            enemyType = EnemyType.regular;
+        else if(enemyTypeGenerator <= 88)
+            enemyType = EnemyType.fire;
+        else if (enemyTypeGenerator <= 91)
+            enemyType = EnemyType.earth;
+        else if (enemyTypeGenerator <= 94)
+            enemyType = EnemyType.wind;
+        else if (enemyTypeGenerator <= 97)
+            enemyType = EnemyType.water;
+        else if (enemyTypeGenerator <= 100)
+            enemyType = EnemyType.lightning;
+
+        switch (enemyType)
+        {
+            case EnemyType.fire:
+                Instantiate(enemyAuras[0], enemyTypeVFX.transform);
+                break;
+            case EnemyType.earth:
+                //Instantiate(enemyAuras[1], enemyTypeVFX.transform);
+                break;
+            case EnemyType.wind:
+                //Instantiate(enemyAuras[2], enemyTypeVFX.transform);
+                break;
+            case EnemyType.water:
+                Instantiate(enemyAuras[3], enemyTypeVFX.transform);
+                break;
+            case EnemyType.lightning:
+                Instantiate(enemyAuras[4], enemyTypeVFX.transform);
+                break;
+        }
         enemyMovement = gameObject.GetComponent<EnemyMovement>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        switch (enemyType)
+        {
+            case EnemyType.fire:
+                EmitFire();
+                break;
+            case EnemyType.earth:
+                break;
+            case EnemyType.wind:
+                break;
+            case EnemyType.water:
+                break;
+            case EnemyType.regular:
+                break;
+        }
+
         if(isDead)
         {
             maskDissolve.position = enemyMovement.PointBelowEnemyHips();
@@ -40,8 +111,6 @@ public class EnemyAttributes : MonoBehaviour
             isDead = true;
             StartCoroutine(KillEnemy());
             //Add some obvious fluidity
-            //coroutines help
-            //Destroy(this.gameObject);
         }
     }
 
@@ -59,7 +128,13 @@ public class EnemyAttributes : MonoBehaviour
         ie.PlayEffect();
         //Turn everything off and ragdoll
         
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(2.2f);
         Destroy(this.gameObject);
+    }
+
+
+    private void EmitFire()
+    {
+
     }
 }
